5c01d12dbddcd35a525b2d0bca2ca5470ad57807
fdsquadbdg <42709958+fdsquadbdg@users.noreply.github.com>
2018-08-26 15:47:36 +0700

    Add files via upload

--- fdsquad_bot.py
+++ fdsquad_bot.py
@@ -152,7 +152,9 @@ while True:
                             else:
                                 cctv['sidermem'][op.param1] += "\n~ " + Name
                                 pref=['eh ada om','hai om','nah','lg ngapain om','halo om','sini om']
-                                client.sendText(op.param1, str(random.choice(pref))+ @Name +' jangan lupa cek pengumuman yaaaa')
+                                msg = new message();
+                                msg.contentMetadata ={'MENTION':'{"MENTIONEES":['+Name+']}'}
+                                client.sendText(op.param1, str(random.choice(pref))+ msg +' jangan lupa cek pengumuman yaaaa')
                         else:
                             pass
                     else:
